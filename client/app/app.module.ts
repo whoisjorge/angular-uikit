04fa76ab56d65f95a8f4185933a9ee2f0dc3838e
Jorge Moliner <email@whoisjorge.me>
2017-12-22 20:59:29 +0100

    fix IE11 support > playground polyfill

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Router
